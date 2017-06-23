5c6e77a4af57ce5ac745f8f62f5bcacf01381ecc
Jeff <200006506@qq.com>
2017-06-23 15:46:32 +0800

    Add graphql client

--- apps/trading_system_web/assets/js/app.js
+++ apps/trading_system_web/assets/js/app.js
@@ -1,5 +1,28 @@
 import "phoenix_html"
+import ApolloClient, { createNetworkInterface } from "apollo-client"
+import gql from "graphql-tag"
 import echarts from "echarts"
 
-const my_chart = echarts.init(document.getElementById("charts"))
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: "/api",
+  }),
+});
+
+client.query({
+  query: gql`
+    query {
+      usStocks(symbol: "FB") {
+        date
+        open_price
+        close_price
+        lowest_price
+        highest_price
+        turnover_vol
+      }
+    }
+  `
+})
+.then(resp => console.log(resp.data.usStocks))
+.catch(error => console.error(error))
 
