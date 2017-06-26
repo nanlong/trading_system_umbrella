affa51c0b926621401f37fcff4cf24f4a969d6e6
Jeff <200006506@qq.com>
2017-06-26 11:53:54 +0800

    Fix test

--- apps/trading_system_web/test/trading_system_web/controllers/page_controller_test.exs
+++ apps/trading_system_web/test/trading_system_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TradingSystem.Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "交易系统"
   end
 end
