36d44b0c1569043cf193c008014b89798586725e
Jeff <200006506@qq.com>
2017-06-30 11:38:28 +0800

    Add TradingCrawler

---
+++ apps/trading_crawler/lib/trading_crawler.ex
@@ -0,0 +1,18 @@
+defmodule TradingCrawler do
+  @moduledoc """
+  Documentation for TradingCrawler.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TradingCrawler.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/trading_crawler/test/trading_crawler_test.exs
@@ -0,0 +1,8 @@
+defmodule TradingCrawlerTest do
+  use ExUnit.Case
+  doctest TradingCrawler
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
