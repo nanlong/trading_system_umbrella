d252fd1e262f7df423227ed8659f9e1d6dc72787
Jeff <200006506@qq.com>
2017-07-14 16:45:00 +0800

    Update js

--- apps/trading_system_web/assets/js/components/stock_state_list.js
+++ apps/trading_system_web/assets/js/components/stock_state_list.js
@@ -5,17 +5,11 @@ import { Table } from 'antd'
 class StockStateList extends React.Component {
 
   componentDidUpdate({data: {refetch}}, state) {
-    console.log('refetch')
     setTimeout(() => refetch(), 1000)
   }
 
   diff(a) {
-    if (a.price < a.dcu60) {
-      return -(1 - a.price / a.dcu60)
-    }
-    else {
-      return a.price / a.dcu60 - 1
-    }
+    return a.price < a.dcu60 ? -(1 - a.price / a.dcu60) : a.price / a.dcu60 - 1
   }
 
   round(value, precision) {
@@ -23,12 +17,16 @@ class StockStateList extends React.Component {
     return Math.round(value * multiplier) / multiplier;
   }
 
+  onRowClick(record, index, event) {
+    window.open(`/stocks/${record.symbol}`)
+  }
+
   render() {
     const dataSource = this.props.data.stockState
     const columns = [
       {title: '股票代码', dataIndex: 'symbol', key: 'symbol'},
       {title: '公司名称', dataIndex: 'stock.cname', key: 'stock.cname'},
-      {title: '行业', dataIndex: 'stock.category', key: 'stock.category'},
+      {title: '行业', dataIndex: 'stock.category', key: 'stock.category', render: (text, record, index) => text ? text : '--'},
       {title: 'ATR', dataIndex: 'atr20', key: 'atr20'},
       {title: '20日最低', dataIndex: 'dcl20', key: 'dcl20', render: (text, record, index) => `$${text}`},
       {title: '60日最高', dataIndex: 'dcu60', key: 'dcu60', render: (text, record, index) => `$${text}`},
@@ -39,7 +37,7 @@ class StockStateList extends React.Component {
     return (
       <div>
         <h1>{dataSource ? dataSource.length : 0} 个符合条件的股票</h1>
-        <Table dataSource={dataSource} columns={columns} rowKey="symbol" />
+        <Table dataSource={dataSource} columns={columns} rowKey="symbol" onRowClick={this.onRowClick} />
       </div>
     )
   }
