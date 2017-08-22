0b828c56522f5c3766c6898f974093c3abd6454a
Jeff <200006506@qq.com>
2017-08-22 17:51:12 +0800

    Add trading_task

---
+++ apps/trading_task/lib/trading_task.ex
@@ -0,0 +1,18 @@
+defmodule TradingTask do
+  @moduledoc """
+  Documentation for TradingTask.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TradingTask.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/trading_task/test/trading_task_test.exs
@@ -0,0 +1,8 @@
+defmodule TradingTaskTest do
+  use ExUnit.Case
+  doctest TradingTask
+
+  test "greets the world" do
+    assert TradingTask.hello() == :world
+  end
+end
