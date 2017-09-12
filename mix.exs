43634935fcdd8531067318a33530123b175146cb
jackalcooper <jackalcooper@gmail.com>
2017-09-12 16:19:26 +0800

    dumb commit

--- mix.exs
+++ mix.exs
@@ -20,6 +20,7 @@ defmodule TradingSystem.Umbrella.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [
+    ]
   end
 end
